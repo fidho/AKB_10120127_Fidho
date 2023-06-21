ed89a612e2934f1f2df45747a56f6b464ba46416
fidho <72368626+fidho@users.noreply.github.com>
2023-06-21 16:52:03 +0700

    Add files via upload

---
+++ 10120127_Fidho/10120127_Fidho/app/src/test/java/com/example/a10120127_fidho/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.a10120127_fidho;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
